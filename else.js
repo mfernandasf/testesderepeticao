7ec9f7f76085ebd992f4999ff6cddc07813617ca
mariafernanda <mfgamep@gmail.com>
2023-06-13 21:56:39 -0300

    testes_estruturas_de_repeticao_js

---
+++ else.js
@@ -0,0 +1,12 @@
+var operacao = '+';
+if(operacao == '+'){
+  alert('adição');
+}else if(operacao == '-'){
+  alert('subtração');
+}else if(operacao == '*'){
+  alert('multiplicação');
+}else if(operacao == '/'){
+  alert('divisão');
+}else{
+  alert('operação não definida');
+}
